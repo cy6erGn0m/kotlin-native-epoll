e396f24161fbd070601f51c36e52bd1e915d6e66
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-04-14 14:37:59 +0300

    SocketChannel: support async connect

--- src/SocketChannel.kt
+++ src/SocketChannel.kt
@@ -1,16 +1,10 @@
 package kotlinx.native.nio
 
-import errno.*
-import epoll.*
 import kotlinx.cinterop.*
+import epoll.*
+import errno.*
 
-class SocketChannel internal constructor(val fd: Int) : ReadableByteChannel, WritableByteChannel {
-    constructor() : this(socket(AF_INET, SOCK_STREAM, 0)) //.ensureUnixCallResult { it >= 0 })
-
-    private var tmpBuffer: DirectByteBuffer? = null
-    
-    override val isOpen: Boolean get() = true   
-
+class SocketChannel : SocketChannelBase() {
     fun connect(address: InetSocketAddress): Boolean {
         val addr = address.address as Inet4Address
         val v = htonl((addr.addr[0].toInt() shl 24) or (addr.addr[1].toInt() shl 16) or (addr.addr[2].toInt() shl 8) or (addr.addr[3].toInt()))
@@ -31,86 +25,18 @@ class SocketChannel internal constructor(val fd: Int) : ReadableByteChannel, Wri
         }
     }
 
-    // TODO finishConnect for non-blocking mode
-    // TODO configureBlocking
+    fun finishConnect(): Boolean {
+        return memScoped {
+            val err = alloc<IntVar>()
+            val size = alloc<socklen_tVar>()
 
-    override fun read(buffer: ByteBuffer): Int {
-        val size = buffer.remaining()
+            getsockopt(fd, SOL_SOCKET, SO_ERROR, err.ptr, size.ptr).ensureUnixCallResult("getsockopt") { it == 0 }
 
-        val rc: Long
-        if (buffer !is DirectByteBuffer) {
-            val tmpBuffer = direct(size)
-            rc = read(fd, tmpBuffer.array, size.toLong())
-            if (rc > 0L) {
-                tmpBuffer.position(0)
-                tmpBuffer.limit(rc.toInt())
-                buffer.put(tmpBuffer)
-            }
-        } else {
-            rc = read(fd, buffer.array, size.toLong())
-            if (rc > 0L) {
-                buffer.position(buffer.position() + rc.toInt())
+            when (err.value) {
+                EINPROGRESS -> false
+                0 -> true
+                else -> throw Exception("connect failed: ${errorMessage(err.value)} (${err.value})")
             }
         }
-
-        return when {
-            rc == 0L -> -1
-            rc > 0L -> rc.toInt()
-            else -> {
-                val errno = errno()
-                if (errno == EAGAIN || errno == EWOULDBLOCK) {
-                    0
-                } else {
-                    throw Exception("read() failed with $errno: ${errorMessage(errno)}")
-                }
-            }
-        }
-    }
-
-    override fun write(buffer: ByteBuffer): Int {
-        val size = buffer.remaining()
-        var rc: Long
-        
-        if (buffer !is DirectByteBuffer) {
-            val tmp = direct(size)
-            val oldPosition = buffer.position()
-            tmp.clear()
-            tmp.put(buffer)
-            buffer.position(oldPosition)
-
-            rc = write(fd, tmp.array, size.toLong())
-        } else {
-            rc = write(fd, buffer.array, size.toLong())
-        }
-
-        if (rc > 0L) {
-            buffer.position(buffer.position() + rc.toInt())
-        }
-
-        return when {
-            rc >= 0L -> rc.toInt()
-            else -> {
-                val errno = errno()
-                if (errno == EAGAIN || errno == EWOULDBLOCK) {
-                    0
-                } else throw Exception("write() failed with $errno: ${errorMessage(errno)}")
-            }
-        }
-    }
-    
-    override fun close() {
-        close(fd)
-        tmpBuffer?.close()
-        tmpBuffer = null
-    }
-    
-    private fun direct(size: Int): DirectByteBuffer {
-        val buffer = tmpBuffer
-        val result = if (buffer == null || buffer.capacity() < size) {
-            (nativeHeap.allocDirectBuffer(size) as DirectByteBuffer).apply { tmpBuffer = this }
-        } else buffer
-
-        return result
     }
 }
-
---
+++ src/SocketChannelBase.kt
@@ -0,0 +1,100 @@
+package kotlinx.native.nio
+
+import errno.*
+import epoll.*
+import kotlinx.cinterop.*
+
+open class SocketChannelBase internal constructor(val fd: Int) : ReadableByteChannel, WritableByteChannel {
+    constructor() : this(socket(AF_INET, SOCK_STREAM, 0)) //.ensureUnixCallResult { it >= 0 })
+
+    private var tmpBuffer: DirectByteBuffer? = null
+    
+    override val isOpen: Boolean get() = true   
+
+    fun configureBlocking(blocking: Boolean) {
+        var flags = fcntl(fd, F_GETFL, 0).ensureUnixCallResult("fcntl(F_GETFL)") { it >= 0 }
+        flags = if (blocking) (flags and O_NONBLOCK.inv()) else (flags or O_NONBLOCK)
+
+        fcntl(fd, F_SETFL, flags).ensureUnixCallResult("fcntl(F_SETFL)") { it == 0 }
+    }
+
+    override fun read(buffer: ByteBuffer): Int {
+        val size = buffer.remaining()
+
+        val rc: Long
+        if (buffer !is DirectByteBuffer) {
+            val tmpBuffer = direct(size)
+            rc = read(fd, tmpBuffer.array, size.toLong())
+            if (rc > 0L) {
+                tmpBuffer.position(0)
+                tmpBuffer.limit(rc.toInt())
+                buffer.put(tmpBuffer)
+            }
+        } else {
+            rc = read(fd, buffer.array, size.toLong())
+            if (rc > 0L) {
+                buffer.position(buffer.position() + rc.toInt())
+            }
+        }
+
+        return when {
+            rc == 0L -> -1
+            rc > 0L -> rc.toInt()
+            else -> {
+                val errno = errno()
+                if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                    0
+                } else {
+                    throw Exception("read() failed with $errno: ${errorMessage(errno)}")
+                }
+            }
+        }
+    }
+
+    override fun write(buffer: ByteBuffer): Int {
+        val size = buffer.remaining()
+        var rc: Long
+        
+        if (buffer !is DirectByteBuffer) {
+            val tmp = direct(size)
+            val oldPosition = buffer.position()
+            tmp.clear()
+            tmp.put(buffer)
+            buffer.position(oldPosition)
+
+            rc = write(fd, tmp.array, size.toLong())
+        } else {
+            rc = write(fd, buffer.array, size.toLong())
+        }
+
+        if (rc > 0L) {
+            buffer.position(buffer.position() + rc.toInt())
+        }
+
+        return when {
+            rc >= 0L -> rc.toInt()
+            else -> {
+                val errno = errno()
+                if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                    0
+                } else throw Exception("write() failed with $errno: ${errorMessage(errno)}")
+            }
+        }
+    }
+    
+    override fun close() {
+        close(fd)
+        tmpBuffer?.close()
+        tmpBuffer = null
+    }
+    
+    private fun direct(size: Int): DirectByteBuffer {
+        val buffer = tmpBuffer
+        val result = if (buffer == null || buffer.capacity() < size) {
+            (nativeHeap.allocDirectBuffer(size) as DirectByteBuffer).apply { tmpBuffer = this }
+        } else buffer
+
+        return result
+    }
+}
+
